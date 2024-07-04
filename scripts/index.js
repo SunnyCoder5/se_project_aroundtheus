478f952627de34424f99535ef62acc9085f77898
Mariia Chudakov <masterova260989@gmail.com>
2024-07-04 19:48:05 +0300

    add js file

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+let initialCards = [
+{
+  name: "Garden of the Gods Road",
+  link: url(unsplash.com/photos/a-large-rock-formation-in-the-middle-of-a-desert-jqKy_DCnqRg)
+},
+
+{ name: "New York City",
+  link: url(unsplash.com/photos/a-view-of-a-city-at-night-from-the-top-of-a-building-wxskcYSJ570)
+},
+
+{name: "Pikes Peak",
+    link: url(unsplash.com/photos/a-mountain-covered-in-snow-under-a-cloudy-sky-nWxZ6COgFr0)
+},
+
+{name: "Santa Barbara West Campus",
+    link: url(unsplash.com/photos/green-grass-near-body-of-water-during-daytime-Wi7zEg3fKq0)
+},
+
+{name: "Monument Valley",
+    link: url(unsplash.com/photos/brown-rock-formation-under-blue-sky-during-daytime-yht4AxGHS8Y)
+},
+
+{name: "Lucia Falls",
+    link: url(unsplash.com/photos/a-river-flowing-through-a-lush-green-forest-KnEXbwSfqRo)
+}
+]
+
