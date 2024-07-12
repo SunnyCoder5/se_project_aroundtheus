557478d5c6e5170a428f893d4cdfbf712ea88c83
Mariia Chudakov <masterova260989@gmail.com>
2024-07-12 19:23:08 +0300

    add editing of form fields, rendering cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,80 @@
 const initialCards = [
   {
     name: "Antelope Canyon",
-    link: "https://unsplash.com/photos/antelope-canyon-arizona-4VeSnxkrNAM",
+    link: "https://images.unsplash.com/photo-1473456229365-7a538630163b?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
     name: "New York City",
-    link: "https://unsplash.com/photos/photo-of-gray-building-USrZRcRS2Lw",
+    link: "https://images.unsplash.com/photo-1448317971280-6c74e016e49c?q=80&w=1832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
     name: "Salt Lake City",
-    link: "https://unsplash.com/photos/green-trees-near-body-of-water-during-daytime-GBrCu6h8H1Y",
+    link: "https://images.unsplash.com/photo-1584255391936-f4d73b095371?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
     name: "Santa Barbara West Campus",
-    link: "https://unsplash.com/photos/people-walking-on-beach-during-daytime-qN2UhvqAU3Y",
+    link: "https://images.unsplash.com/photo-1590255041502-9a2603c6fd39?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
     name: "Monument Valley",
-    link: "https://unsplash.com/photos/three-mountains-at-desert-during-daytime-VpmPEs0yNYk",
+    link: "https://images.unsplash.com/photo-1558353016-37b89cf18040?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
     name: "Lucia Falls",
-    link: "https://unsplash.com/photos/a-river-flowing-through-a-lush-green-forest-KnEXbwSfqRo",
+    link: "https://images.unsplash.com/photo-1709336860701-5ac2d825a170?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
 const profileEditButton = document.querySelector("#profile-edit-button");
-
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalCloseButton = document.querySelector("#modal-close-button");
-
-profileEditButton.addEventListener("click", () => {
-  console.log("button clicked");
+const profileTitle = document.querySelector("#profile-title");
+const profileDescription = document.querySelector("#profile-description");
+const profileTitleInput = document.querySelector("#modal-form-input-name");
+const profileDescriptionInput = document.querySelector(
+  "#modal-form-input-description"
+);
+const profileFormElement = profileEditModal.querySelector(
+  "#modal-form-element"
+);
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector("#card-list");
+profileEditButton.addEventListener("click", function () {
+  profileTitleInput.value = profileTitle.textContent.trim();
+  profileDescriptionInput.value = profileDescription.textContent.trim();
   profileEditModal.classList.add("modal_opened");
 });
 
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
+
+  cardTitleElement.textContent = data.name;
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
+  return cardElement;
+}
+
+profileFormElement.addEventListener("submit", handleFormSubmit);
+
 modalCloseButton.addEventListener("click", function () {
   profileEditModal.classList.remove("modal_opened");
+});
+
+initialCards.forEach(function (data) {
+  const cardElement = getCardElement(data);
+  cardList.prepend(cardElement);
 });
